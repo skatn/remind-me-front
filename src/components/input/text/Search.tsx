f6fb52309af5f14cc5ad70a2bc5e52d559d962f8
skatn <87296704+skatn@users.noreply.github.com>
2024-06-21 18:44:21 +0900

    feat: 문제집 검색 기능 적용 (#5)

--- src/components/input/text/Search.tsx
+++ src/components/input/text/Search.tsx
@@ -4,9 +4,16 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 interface SearchProps {
   placeholder?: string;
   className?: string;
+  onChange?: (value: string) => void;
+  value?: string;
 }
 
-const Search = ({ placeholder = 'Search', className }: SearchProps) => {
+const Search = ({
+  placeholder = 'Search',
+  className,
+  onChange,
+  value,
+}: SearchProps) => {
   return (
     <div
       className={`flex items-center gap-[16px] rounded-[24px] bg-neutral-light-4 px-[16px] py-[12px] ${className}`}
@@ -15,6 +22,10 @@ const Search = ({ placeholder = 'Search', className }: SearchProps) => {
       <input
         placeholder={placeholder}
         className="bg-transparent text-body-md flex-grow"
+        value={value}
+        onChange={(e) =>
+          onChange ? onChange(e.currentTarget.value) : () => {}
+        }
       />
     </div>
   );
--- src/pages/SubjectListPage.tsx
+++ src/pages/SubjectListPage.tsx
@@ -4,16 +4,23 @@ import Button from '../components/input/button/Button';
 import Search from '../components/input/text/Search';
 import Subject from '../components/subject/Subject';
 import useFetchSubjectList from '../hooks/subject/useFetchSubjectList';
+import { useState } from 'react';
+import { SubjectListGetRequest } from '../types/subjectType';
 
 const SubjectListPage = () => {
-  const { content: subjects, ref } = useFetchSubjectList({ size: 1 });
+  const [request, setRequest] = useState<SubjectListGetRequest>({ size: 1 });
+  const { content: subjects, ref } = useFetchSubjectList(request);
 
   return (
     <div className="flex flex-col">
       <Navitagion title="문제집" right={<BackButton />} />
       <div className="mt-[12px] flex items-center gap-[24px] px-[24px]">
         <Button className="h-[38px]">추가</Button>
-        <Search className="flex-grow" />
+        <Search
+          className="flex-grow"
+          value={request.title}
+          onChange={(title) => setRequest((prev) => ({ ...prev, title }))}
+        />
       </div>
       <div className="mt-[24px] grid grid-cols-2 gap-[24px] px-[24px] pb-[24px]">
         {subjects.map((subject) => (
