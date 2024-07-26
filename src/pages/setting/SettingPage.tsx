ad977cd6029798263a80a2f75b7424e285d39560
skatn <87296704+skatn@users.noreply.github.com>
2024-07-26 15:34:26 +0900

    feat: 설정 페이지 UI 구현 (#19)

---
+++ src/pages/setting/SettingPage.tsx
@@ -0,0 +1,22 @@
+import Navigation from '../../components/navigation/Navigation';
+import BackButton from '../../components/navigation/BackButton';
+import Divider from '../../components/divider/Divider';
+
+const SettingPage = () => {
+  return (
+    <div className="flex flex-col">
+      <Navigation title="설정" left={<BackButton />} />
+      <div className="m-[24px] flex flex-col gap-[4px]">
+        <span className="cursor-pointer rounded-md p-[10px] hover:bg-neutral-light-4">
+          로그아웃
+        </span>
+        <Divider />
+        <span className="cursor-pointer rounded-md p-[10px] hover:bg-neutral-light-4">
+          회원 탈퇴
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default SettingPage;
